897252969be15d3ef51b71fdb66195dd3fcf0f16
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-13 14:17:30 +0530

    feat: websocket client added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [message, setMessage] = useState("");
+  const [socket, setSocket] = useState();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(!socket) return;
+    socket.send(message);
+  };
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000");
+    setSocket(ws);
+
+    ws.onmessage = (e) => {
+      alert(e.data);
+    }
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)}/>
+        <button>Send</button>
+      </form>
     </>
   )
 }
